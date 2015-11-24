f180d974cabe6ae2cf6f1b80f3007c9e5a3a74b1
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-11-24 13:33:24 -0500

    First working version First version doesn't do any saving amidst an edit session -- only saves when the relevant flag-file is deleted so as to signify that the edit session is closed. Some uses of this program may be impaired by this limitation -- however, it does not affect the use which is this program's initial raison-d'etre, that being it's use in triggered-from-remote scripts that do GIT commits.

---
+++ inner-wrap.pl
@@ -0,0 +1,100 @@
+use strict;
+use File::Basename;
+use argola;
+use chobak_fnm;
+
+my $callidvr;
+my $outerfile;
+my $outerfdir;
+my $outerfbas;
+my $outerfext;
+my $outerfok;
+my $theftype;
+my $nfsedir;
+
+my $edbase;
+my $edflag;
+my $edfile;
+my $cmdon;
+
+
+
+# Now find out where everything is stored:
+$nfsedir = $ENV{"NFS_EDIT_PRG_DIR"};
+if ( $nfsedir eq "" )
+{
+  die "\n"
+  . "Failed to specify edit directory environment variable:\n"
+  . "  NFS_EDIT_PRG_DIR:\n"
+  . "\n";
+}
+
+if ( !( -d $nfsedir ) )
+{
+  die "\n"
+  . "No such directory:\n"
+  . "  " . $nfsedir . ":\n"
+  . "Check environment variable: NFS_EDIT_PRG_DIR:\n"
+  . "\n";
+}
+
+
+
+# Establish CallID Variable:
+sleep(2); # Just to prevent the last remaining possible cause of duplication:
+$callidvr = `date +%s`; chomp($callidvr);
+$callidvr .= "-" . $$;
+
+#system("echo",$callidvr);
+
+if ( !(&argola::yet()) )
+{
+  die "\nNo target file specified:\n\n";
+}
+
+$outerfile = &argola::getrg();
+$outerfdir = dirname($outerfile);
+
+if ( ! ( -d ( $outerfdir ) ) )
+{
+  die "\nNo directory containing file:\n    " . $outerfile . ":\n\n";
+}
+
+
+# Now we figure out what the file type is:
+
+$outerfbas = $outerfile;
+$outerfok = &chobak_fnm::oneext($outerfbas,$outerfext);
+$theftype = "txt";
+if ( $outerfok ) { $theftype = $outerfext; }
+
+
+$edbase = $nfsedir . "/" . $callidvr . "-b.txt";
+$edflag = $nfsedir . "/" . $callidvr . "-f.txt";
+$edfile = $nfsedir . "/" . $callidvr . "-e." . $theftype;
+
+
+if ( -f $outerfile )
+{
+  system("cp",$outerfile,$edfile);
+}
+$cmdon = "| cat >";
+&argola::wraprg_lst($cmdon,$edbase);
+open TAK, $cmdon;
+print TAK $theftype . "\n";
+close TAK;
+system("touch",$edflag);
+
+while ( -f $edflag )
+{
+  sleep(2);
+}
+system("cp",$edfile,$outerfile);
+
+
+
+system("rm","-rf",$edbase,$edflag,$edfile);
+
+
+
+
